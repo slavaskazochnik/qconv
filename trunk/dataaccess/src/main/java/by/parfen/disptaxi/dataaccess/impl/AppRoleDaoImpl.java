ff1166c5cec48805bd556bb43379df5e12ff4c62
parfen <parfen@9fe200af-88fe-f038-dce2-638ed1438289>
2015-03-22 19:49:01 +0000

    1) edited main POM (dependencies) 2) edited dataacess POM (dependencies) 3) added dataacess interface AppRoleDao.java for AppRole with impl/AppRoleDaoImpl.java

---
+++ trunk/dataaccess/src/main/java/by/parfen/disptaxi/dataaccess/impl/AppRoleDaoImpl.java
@@ -0,0 +1,17 @@
+package by.parfen.disptaxi.dataaccess.impl;
+
+import org.springframework.stereotype.Repository;
+
+import by.parfen.disptaxi.dataaccess.AppRoleDao;
+import by.parfen.disptaxi.datamodel.AppRole;
+
+@Repository
+public class AppRoleDaoImpl implements AppRoleDao {
+
+	@Override
+	public void save(AppRole appRole) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
