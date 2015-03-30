60bfd62a9995fbb72ca97fd52c8fec05cb8c1cd2
parfen <parfen@9fe200af-88fe-f038-dce2-638ed1438289>
2015-03-30 19:58:41 +0000

    added EM annotations for simple Tables without FKs

--- trunk/datamodel/src/main/java/by/parfen/disptaxi/datamodel/OrdersResult.java
+++ trunk/datamodel/src/main/java/by/parfen/disptaxi/datamodel/OrdersResult.java
@@ -1,26 +1,42 @@
 package by.parfen.disptaxi.datamodel;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class OrdersResult {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column
 	private String name;
+	@Column
 	private String descr;
 
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescr() {
 		return descr;
 	}
+
 	public void setDescr(String descr) {
 		this.descr = descr;
 	}
--- trunk/datamodel/src/main/java/by/parfen/disptaxi/datamodel/UsersAccount.java
+++ trunk/datamodel/src/main/java/by/parfen/disptaxi/datamodel/UsersAccount.java
@@ -12,55 +12,69 @@ public class UsersAccount {
 	private Person person;
 	private Long signActive;
 	private Date dCreate;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassw() {
 		return passw;
 	}
+
 	public void setPassw(String passw) {
 		this.passw = passw;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public AppRole getAppRole() {
 		return appRole;
 	}
+
 	public void setAppRole(AppRole appRole) {
 		this.appRole = appRole;
 	}
+
 	public Person getPerson() {
 		return person;
 	}
+
 	public void setPerson(Person person) {
 		this.person = person;
 	}
+
 	public Long getSignActive() {
 		return signActive;
 	}
+
 	public void setSignActive(Long signActive) {
 		this.signActive = signActive;
 	}
+
 	public Date getdCreate() {
 		return dCreate;
 	}
+
 	public void setdCreate(Date dCreate) {
 		this.dCreate = dCreate;
 	}
-	
-	
+
 }
