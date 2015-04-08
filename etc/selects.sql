01a95a939c50b6c6671f3b28da84e45d3916f3ec
parfen <parfen@9fe200af-88fe-f038-dce2-638ed1438289>
2015-04-08 18:56:49 +0000

    added try sections for testing duplicate values

---
+++ etc/selects.sql
@@ -0,0 +1,11 @@
+select * from test.app_role a;
+select * from test.user_account u;
+select * from test.user_role u;
+select * from test.user_profile u;
+
+select * from test.car c;
+select * from test.driver d;
+select * from test.customer c;
+
+select * from test.city c;
+select * from test.street s;
